6563b6f8ac12a0acdf048e60d3b2996ce4f6d0bf
mromani <mromani@redhat.com>
2018-08-09 15:55:35 +0200

    add jenkins pipeline

---
+++ pipelines/pipeline.groovy
@@ -0,0 +1,66 @@
+def target_cluster_flags = ""
+
+pipeline {
+    agent any
+    stages{
+        stage('prepare') {
+            steps {
+                sh 'printenv'
+                script {
+                    if (!"${BUILD_TAG}"?.trim()) {
+                        currentBuild.result = 'ABORTED'
+                        error('Tag to build is empty')
+                    }
+                    echo "Releasing tag ${BUILD_TAG}"
+
+                    //target_cluster_flags = "--server=$ocp_cluster_url --insecure-skip-tls-verify"
+                }
+            }
+        }
+        stage('Build') {
+            stages {
+                stage('Source checkout') {
+                    steps {
+                        checkout(
+                            [$class                           : 'GitSCM', branches: [[name: "refs/tags/${BUILD_TAG}"]],
+                                doGenerateSubmoduleConfigurations: false,
+                                extensions                       : [],
+                                submoduleCfg                     : [],
+                                userRemoteConfigs                : [[credentialsId: "${GIT_CREDENTIAL_ID}", url: "${GIT_URL}"]]]
+                        )
+                    }
+                }
+                stage('Maven') {
+                    steps {
+                        withMaven(mavenSettingsFilePath: "${MVN_SETTINGS}") {
+                            sh "mvn -f ${POM_FILE} versions:set -DnewVersion=${BUILD_TAG}"
+                        }
+                    }
+                }
+                stage('Publish on nexus') {
+                    echo "SKIPPED"
+                    steps {
+                        if(${DEPLOY_ON_NEXUS}){
+                            withMaven(mavenSettingsFilePath: "${MVN_SETTINGS}") {
+                                sh "mvn -f ${POM_FILE} clean deploy -Dmaven.javadoc.skip=true -DskipTests "
+                            }
+                        }else{
+                            script{
+                                echo "SKIPPED"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        stage('OCP Deploy'){
+            steps {
+                sh """ 
+                    mkdir ${WORKSPACE}/deployments
+                    cp ${WORKSPACE}/web-app/target/ROOT.war ${WORKSPACE}/deployments
+                    oc start-build eap-web --from-dir=${WORKSPACE}/deployments
+                """
+            }
+        }
+    }
+}
