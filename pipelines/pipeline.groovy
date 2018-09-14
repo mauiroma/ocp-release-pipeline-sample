c85db2246a494d1b010bf46cd57bb03c2e237d37
mauiroma <maurizio.romani@gmail.com>
2018-09-14 11:58:22 +0200

    fix pipeline

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -118,7 +118,7 @@ pipeline {
                 stage('Publish on nexus') {
                     steps {
                         script{                            
-                            if(Boolean.parseBoolean(env.DEPLOY_ON_NEXUS}){
+                            if(Boolean.parseBoolean(env.DEPLOY_ON_NEXUS)){
                                 echo "DEPLOY ON NEXUS"
                                 withMaven(mavenSettingsFilePath: "${MVN_SETTINGS}") {
                                     sh "mvn -f ${POM_FILE} clean deploy -Dmaven.javadoc.skip=true -DskipTests "
