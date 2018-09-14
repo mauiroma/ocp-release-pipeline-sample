8d51ac6d4ae0d302ce7ae1750cc484cc9abc6a99
mauiroma <maurizio.romani@gmail.com>
2018-09-14 11:44:19 +0200

    fix pipeline

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -93,7 +93,7 @@ pipeline {
                         stage('SonarQube analysis') {
                             steps {
                                 script{
-                                    if(env.SONAR){
+                                    if(Boolean.parseBoolean(env.SONAR)){
                                         withSonarQubeEnv('Sonar-MacLocalhost') {
                                             withMaven(mavenSettingsFilePath: "${MVN_SETTINGS}") {
                                               sh "mvn -f ${POM_FILE} sonar:sonar"
